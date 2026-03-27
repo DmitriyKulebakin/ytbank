dbc023ced034c262c43fa2ee71e2cc3143ea51dd
sindat <dimakuleb18@gmail.com>
2026-03-27 22:14:01 +0300

    Initial commit

---
+++ src/main/kotlin/sindat/pets/ytbank/YtbankApplication.kt
@@ -0,0 +1,11 @@
+package sindat.pets.ytbank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YtbankApplication
+
+fun main(args: Array<String>) {
+	runApplication<YtbankApplication>(*args)
+}
---
+++ src/test/kotlin/sindat/pets/ytbank/YtbankApplicationTests.kt
@@ -0,0 +1,13 @@
+package sindat.pets.ytbank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YtbankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

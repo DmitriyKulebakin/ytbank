30398cb184d67b451e4a9784ce81449eb05b08e8
sindat <dimakuleb18@gmail.com>
2026-03-31 21:43:28 +0300

    validation updated

--- src/main/kotlin/sindat/pets/ytbank/users/obj/User.kt
+++ src/main/kotlin/sindat/pets/ytbank/users/obj/User.kt
@@ -1,15 +1,25 @@
 package sindat.pets.ytbank.users.obj
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Null
+import jakarta.validation.constraints.PastOrPresent
 import java.time.LocalDateTime
 
 data class User(
+    @Null
     var id: Long,
+    @NotNull
     var username: String,
+    @NotNull
     var passwordHash: String, // 60-255 hash
     var email: String?,
+    @NotNull
     var gender: Gender,
+    @PastOrPresent
     var firstSeenDate: LocalDateTime?,
+    @PastOrPresent
     var lastSeenDate: LocalDateTime?,
-    var permissionLevel: Permission,
-    var accountAmount: Long,
+    @Null
+    var permissionLevel: Permission?,
+    var accountAmount: Long,  //TODO can provide any balance on start!!
 )
